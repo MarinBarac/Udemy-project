24249bff43d630b30b0f9da2bc0f78bd1e517b5d
lovricavericafesb <marin.barac.00@fesb.hr>
2021-05-18 12:36:24 +0200

    First Commit

---
+++ shared.js
@@ -0,0 +1,41 @@
+var backdrop = document.querySelector('.backdrop');
+var selectPlanButtons = document.querySelectorAll('.plan button');
+var modal = document.querySelector('.modal');
+var modalNoButton = document.querySelector('.modal__action--negative')
+var toggleButton = document.querySelector('.toggle-button');
+var mobileNav = document.querySelector('.mobile-nav');
+
+//console.dir(selectPlanButtons);
+for (var i = 0; i < selectPlanButtons.length; i++){
+    selectPlanButtons[i].addEventListener('click', function() {
+        //modal.style.display = 'block';
+        //backdrop.style.display = 'block';
+        //modal.className = 'open' //This will actually overwrite the complete class
+        modal.classList.add('open');
+        backdrop.classList.add('open');
+    });
+}
+
+backdrop.addEventListener('click', function() {
+    mobileNav.classList.remove('open');
+    closeModal();
+});
+
+
+if (modalNoButton) {
+    modalNoButton.addEventListener('click', closeModal);
+}
+
+function closeModal(){
+    //backdrop.style.display = 'none';
+    //modal.style.display = 'none';
+    if (modal){
+        modal.classList.remove('open');
+    }
+    backdrop.classList.remove('open');
+}
+
+toggleButton.addEventListener('click', function() {
+    mobileNav.classList.add('open');
+    backdrop.classList.add('open');
+})
